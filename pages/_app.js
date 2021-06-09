9b2b533a17694ddb7b7e07aba123bb25b44f2e26
Desmond Nyamador <dselasi4u@gmail.com>
2021-06-09 12:06:19 +0000

    Animate on scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
+import AOS from 'aos'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+      AOS.init({
+        easing: "ease-out-cubic",
+        once: true, 
+        offset: 50
+      })
+  },[])
+
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from 'next/head'
 import Hero from '../containers/Hero'
 import Reasons from '../containers/Reasons'
+import Footer from '../containers/Footer'
+import MobileVersions from '../containers/MobileVersions'
+import Features from '../containers/Features'
+import ManageYourSchool from '../containers/ManageYourSchool'
 
 export default function Home() {
   return (
     <>
         <Hero/>
         <Reasons/>
+        <Features/>
+        <ManageYourSchool/>
+        <MobileVersions/>
+        <Footer/>
     </>
   )
 }
