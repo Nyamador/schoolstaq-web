9d2f7a9cd7a5ac6908eced50a16778acb86bac61
Desmond Nyamador <dselasi4u@gmail.com>
2021-06-15 02:03:25 +0000

    Macbook mockup with feature icons

--- containers/Features.js
+++ containers/Features.js
@@ -1,17 +1,57 @@
+import Image from 'next/image'
+
 
 const Features = () => {
-
+    const FeatureIcons = [
+        {
+            link: "/svg/student_feature.svg",
+            name: ""
+        },
+        {
+            link: "/svg/bill.svg",
+            name: ""
+        },
+        {
+            link: "/svg/analytics.svg",
+            name: ""
+        },
+        {
+            link: "/svg/staff.svg",
+            name: ""
+        },
+        {
+            link: "/svg/attendance.svg",
+            name: ""
+        },        
+        {
+            link: "/svg/notifications.svg",
+            name: ""
+        },
+        {
+            link: "/svg/webpage.svg",
+            name: ""
+        },
+        {
+        link: "/svg/admission_link.svg",
+        name: ""
+    }    
+    ]
+    
 
 
     return (
-        <section className="px-6 py-20 md:px-20 md:py-20 h-screen text-bluetiful bg-bluetiful-light">
+        <section className="px-6 py-20 md:px-20 md:py-20 text-bluetiful bg-bluetiful-light">
             <div className="md:text-center">
                 <h3 data-aos="fade-up" className="text-2xl md:text-4xl font-bold">Our Core Features</h3>
-                <p className="">We provide you with cloud-based infrastructure to secure the data of Africa's next generation.</p>
+                <p className="mb-10">We provide you with cloud-based infrastructure to secure the data of Africa's next generation.</p>
             </div>
 
-            <div className="flex flex-row">
-
+            <div className="grid grid-cols-2 md:grid-cols-4">
+                {
+                    FeatureIcons.map(feature => <div className="flex flex-row justify-center mb-20">
+                            <Image src={feature.link} height="70" width="70"/>
+                        </div>
+                )}
             </div>
             
         </section>
--- containers/ManageYourSchool.js
+++ containers/ManageYourSchool.js
@@ -1,13 +1,14 @@
+import Image from 'next/image'
 
 const ManageYourSchool = () => {
     return (
         <section className="flex flex-col md:flex-row px-6 py-20 md:px-20 md:py-20">
-            <div className="md:w-6/12">
-
+            <div className="md:w-6/12 text-center">
+                    <Image src="/svg/macbook_mockup.svg" height="400" width="400"/>
             </div>
 
             <div className="md:w-6/12">
-                <h3 className="text-2xl md:text-4xl font-bold md:text-center text-bluetiful">Manage Your School From Anywhere</h3>
+                <h3 className="text-2xl md:text-4xl md:mb-10 font-bold text-bluetiful">Manage Your School From Anywhere</h3>
                 <div>
                     <p>We provide you with cloud-based infrastructure to safely secure the data of Africa’s next generation We provide you with cloud-based infrastructure to safely secure the data of Africa’s next generation We provide you with cloud-based infrastructure to safely secure the data of Africa’s next generation</p>
                 </div>
